3ad5b3d31008ca90c2a4d68f96f8f1a4de30ad4f
Abhishek Jain <68477817+abhishekdmc4@users.noreply.github.com>
2026-03-30 22:52:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from langchain_community.llms import OpenAI
 ##from langchain.prompts import PromptTemplate, ChatPromptTemplate
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 import openai
-from langchain.agents import initialize_agent,AgentType,create_react_agent
+from langchain_classic.agents import initialize_agent,AgentType,create_react_agent
 from langchain_community.agent_toolkits.load_tools import load_tools
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage
