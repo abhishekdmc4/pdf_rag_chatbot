50078627924baa233e7e6303e1bd5deea1a773b6
Abhishek Jain <68477817+abhishekdmc4@users.noreply.github.com>
2026-03-31 15:26:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
+
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+
 
 from langchain_core.callbacks import StdOutCallbackHandler
 
