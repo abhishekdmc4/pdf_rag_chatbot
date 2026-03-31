a1a9c89d148b402217314bd877e2f1e11e2113b9
Abhishek Jain <68477817+abhishekdmc4@users.noreply.github.com>
2026-03-31 15:09:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
+
 from langchain_core.callbacks import StdOutCallbackHandler
 
 # --- 1. API Configuration ---
